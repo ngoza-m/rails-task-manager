9660acff2eb3e7c793ae6ec126954e5857baf000
Ngoza Mayaka <ngozamay@gmail.com>
2024-08-14 12:16:50 +0200

    rails task manager

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema[7.1].define(version: 2024_08_13_120803) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "details"
-    t.boolean "completed"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
